fede857b7ce0bcb7121192a99b6ffd351466d202
Juan <jlvives227@gmail.com>
2022-08-28 11:17:40 -0500

    Creating user profile model.

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import profiles_api.models
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -121,3 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = "profiles_api.UserProfile"
